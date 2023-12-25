2f24ebd2f0fe448c643bc9db8e48d283b8be9aa9
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2023-12-25 13:03:16 +0200

    Update dal.test.js

--- dal.test.js
+++ dal.test.js
@@ -2,13 +2,13 @@
 const assert = require('assert')
 const dal = require('./dals/dal')
 
-describe('Testing functionallity of the DAL' , () => {
+describe('Testing functionallity of the DAL', () => {
     beforeEach(async () => {
         await dal.create_table_if_not_exist()
         await dal.delete_all()
-        await dal.new_message({ 'ID': 1,'NAME': 'bot', 'TEXT': 'hello', 'TIME': '00:00:00','SUPER-ID':0})  // Id: 1
-        await dal.new_message({ 'ID': 2,'NAME': 'Teddy', 'TEXT': 'hello', 'TIME': '00:00:00','SUPER-ID':0}) //ID
-        await dal.new_message({ 'ID': 3,'NAME': 'bot', 'TEXT': 'hello', 'TIME': '00:00:00','SUPER-ID':0}) // Id: 3
+        await dal.new_message({ 'ID': 1, 'NAME': 'bot', 'TEXT': 'hello', 'TIME': '00:00:00', 'SUPER-ID': 0 })  // id: 1
+        await dal.new_message({ 'ID': 2, 'NAME': 'Teddy', 'TEXT': 'hello', 'TIME': '00:00:00', 'SUPER-ID': 0 }) //id 2
+        await dal.new_message({ 'ID': 3, 'NAME': 'bot', 'TEXT': 'hello', 'TIME': '00:00:00', 'SUPER-ID': 0 }) // Id: 3
     }, 20000)
 
     it('get_all', async () => {
@@ -28,29 +28,29 @@ describe('Testing functionallity of the DAL' , () => {
     })
 
     it('updated_message', async () => {
-        await dal.update_emplyee(3, { 'NAME': 'MOSHE', 'TEXT': 'hello', 'TIME': '00:00:00','SUPER-ID':0})
+        await dal.update_emplyee(3, { 'NAME': 'MOSHE', 'TEXT': 'hello', 'TIME': '00:00:00', 'SUPER-ID': 0 })
         const expected = 'MOSHE'
         const message_id_3 = await dal.get_by_id(3)
         const actual = message_id_3.NAME
         console.log(actual);
         assert.strictEqual(expected, actual)
-    })    
+    })
 
     it('delete_message', async () => {
         await dal.delete_message(3)
         const expected = undefined
         const message_id_3 = await dal.get_by_id(3)
         assert.strictEqual(expected, message_id_3)
-    })        
+    })
 
     it('new_message', async () => {
-        await dal.new_message({ 'NAME': 'Shuli', 'TEXT': 'hello', 'TIME': '00:00:00','SUPER-ID':0}) // Id: 4
+        await dal.new_message({ 'NAME': 'Shuli', 'TEXT': 'hello', 'TIME': '00:00:00', 'SUPER-ID': 0 }) // Id: 4
         const expected = 'Shuli'
         const message_id_4 = await dal.get_by_id(4)
         assert.strictEqual(expected, message_id_4.NAME)
-    })        
+    })
 
-    
+
     // complete all other tests for all methods:
     // update_emplyee(id, updated_message)
     // delete_message(id)
