1091ce13b00a413a58841511588d210a557b1d4f
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-01-17 20:07:27 +0200

    UP

--- dals/dals_flights/dal_1.js
+++ dals/dals_flights/dal_1.js
@@ -14,76 +14,45 @@ const connectedKnex = knex({
     }
 })
 
-async function create_table_if_not_exist() {
-    const tableExists = await connectedKnex.schema.hasTable('users');
+// ---------------User functions only---------------
 
-    if (!tableExists) {
-        await connectedKnex.schema.createTable('users', (table) => {
-            table.increments('id').primary(); // This creates a SERIAL column
-            table.string('username').notNullable();
-            table.string('password').notNullable();
-            table.string('email').notNullable();
-        });
-    }
+//new_user
+async function sp_i_users(name, email, password) {
+    const new_user = await connectedKnex.raw(`CALL sp_i_users('${name}','${email}','${password}');`)
+    return new_user
+}
+//new_user (Automatically generates a password)
+async function sp_pass_users(name, email) {
+    const new_user = await connectedKnex.raw(`CALL sp_pass_users('${name}','${email}','');`)
+    return new_user 
 }
 
-async function sp_i_users(name,email,password){
-    const result = await connectedKnex.raw(`CALL sp_i_users('${name}','${email}','${password}');`)
-}
-
-async function sp_pass_users(name,email){
-    const result = await connectedKnex.raw(`CALL sp_pass_users('${name}','${email}','');`)    
-      
-}
-async function delete_all() {
-    // db.run('update users ....')
-    const result = await connectedKnex('users').del()
-    await connectedKnex.raw('ALTER SEQUENCE "users_id_seq" RESTART WITH 1');
-    return result
-}
-
-async function getNextUserId() {
+async function update_user(id, emall, password) {
     try {
-      const result = await connectedKnex.raw(`SELECT get_next_user_id()`);
-      return result;
-    } catch (error) {
-      console.error('Error fetching next user ID:', error);
-      throw error; // Re-throw the error for further handling
+        if (emall === null) {
+            const update = await connectedKnex.raw(`CALL update_user_info(${id}, ${emall}, '${password}');`)
+            return update 
+        }
+        if (password === null) {
+            const update = await connectedKnex.raw(`CALL update_user_info(${id}, '${emall}', ${password});`)
+            return update
+        }
     }
-  }
-  
-
-
-
-async function get_all() {
-    // db.run('select * from users')
-    const users = await connectedKnex('users')
-    .select('users.*', 'roles.role_name')
-    .from('users')
-    .join('roles', 'users.role_id', 'roles.id');
-  
-
-    return users
+    catch (e) {
+        throw     console.error('Not carried out:', e);
+        ; // Re-throw the error for further handling
+    }
 }
 
 async function get_by_id(id) {
     // db.run('select * from users where id=?')
-    const user = await connectedKnex('users').select('*').where('id', id).first()
+    const user = await connectedKnex('users')
+    .select('users.*', 'roles.role_name')
+    .join('roles', 'users.role_id', 'roles.id')
+    .where('users.id', id)
+    .first();
+
     return user
-}
-
-async function new_user(new_mes) {
-    // db.run('insert into users ....')
-    // result[0] will be the new ID given by the SQL
-    // Insert into users values(....)
-    const result = await connectedKnex('users').insert(new_mes)
-    return { ...new_mes, id: result[0] }
-}
-
-async function update_user(id, updated_user) {
-    // db.run('update users ....')
-    const result = await connectedKnex('users').where('id', id).update(updated_user)
-    return updated_user
 }
 
 async function delete_user(id) {
@@ -92,7 +61,61 @@ async function delete_user(id) {
     return result
 }
 
+// ---------------Admin permission only---------------
+async function get_all() {
+    // db.run('select * from users')
+    const users = await connectedKnex('users')
+        .select('users.*', 'roles.role_name')
+        .from('users')
+        .join('roles', 'users.role_id', 'roles.id');
+
+    return users
+}
+async function delete_all() {
+    // db.run('update users ....')
+    const result = await connectedKnex('users').del()
+    await connectedKnex.raw('ALTER SEQUENCE "users_id_seq" RESTART WITH 1');
+    return result
+}
+
+// ---------------Test functions only---------------
+
+async function get_next_user_id() {
+    try {
+        let result = await connectedKnex.raw(`SELECT get_next_user_id()`);
+        return result
+
+    } catch (e) {
+        throw    console.error('Error fetching next user ID:', e);
+
+    }
+}
+
+async function set_id_user(id) {
+    try {
+        const result = await connectedKnex.raw(`CALL reset_id_user(${id})`);
+        return result;
+
+    } catch (e) {
+        throw    console.error('Error fetching next user ID:', e);
+
+    }
+}
+// async function create_table_if_not_exist() {
+//     const tableExists = await connectedKnex.schema.hasTable('users');
+
+//     if (!tableExists) {
+//         await connectedKnex.schema.createTable('users', (table) => {
+//             table.increments('id').primary(); // This creates a SERIAL column
+//             table.string('username').notNullable();
+//             table.string('password').notNullable();
+//             table.string('email').notNullable();
+//         });
+//     }
+// }
+
 module.exports = {
-    get_all, get_by_id, new_user, update_user, delete_user,
-    delete_all, create_table_if_not_exist,sp_i_users,sp_pass_users,getNextUserId
+    get_all, get_by_id, update_user, delete_user,
+    delete_all, sp_i_users, sp_pass_users, get_next_user_id, set_id_user
+    // ,create_table_if_not_exist
 }
