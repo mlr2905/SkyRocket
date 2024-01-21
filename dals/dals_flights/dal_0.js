f13ef6306bef39fd280ba74fa34a7889dbda84f1
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-01-21 14:24:07 +0200

    up

--- dals/dals_flights/dal_0.js
+++ dals/dals_flights/dal_0.js
@@ -53,8 +53,9 @@ async function get_all() {
     .leftJoin('airlines', 'airlines.id', 'flights.airline_id')
     .leftJoin('countries as origin_countries', 'origin_countries.id', 'flights.origin_country_id')
     .leftJoin('countries as destination_countries', 'destination_countries.id', 'flights.destination_country_id')
-    .select('flights.*', 'airlines.name as airline_name', 'origin_countries.country_name as origin_country_name', 'destination_countries.country_name as destination_country_name');
-    
+    .leftJoin('planes', 'planes.id', 'flights.plane_id')
+    .select('flights.*', 'airlines.name as airline_name', 'origin_countries.country_name as origin_country_name', 'destination_countries.country_name as destination_country_name','planes.seat as Total tickets')
+       
     const f = await connectedKnex('tickets')  
     .leftJoin('countries', 'countries.id', 'tickets.flight_id')
     .leftJoin('customers', 'customers.id', 'tickets.customer_id')
