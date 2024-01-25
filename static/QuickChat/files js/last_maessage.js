2a0fee181868776bcacf56de1df9942cd2337d9c
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-01-25 22:22:27 +0200

    Update last_maessage.js

--- static/QuickChat/files js/last_maessage.js
+++ static/QuickChat/files js/last_maessage.js
@@ -30,7 +30,7 @@ async function last_message() { //Shows recent messages in chat details with sen
             user_name = last_m[a].user
             imgMap[i].src = "bot.png"
         }
-        h4_son.innerHTML = `${<h4>user_name</h4>}:`
+        h4_son.innerHTML = `<h4>${user_name}:<h4>`
         p_son_time.innerHTML = last_m[a].time
         div2.appendChild(h4_son)
         div2.appendChild(p_son_time)
