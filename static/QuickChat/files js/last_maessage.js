7176b2accaf0db2cf0b7bdc1fa12d98d4d9cc64e
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-01-25 22:50:50 +0200

    Update last_maessage.js

--- static/QuickChat/files js/last_maessage.js
+++ static/QuickChat/files js/last_maessage.js
@@ -10,11 +10,11 @@ async function last_message() { //Shows recent messages in chat details with sen
         let a = last_m.length - 1
         let div = document.getElementById(`p-${i}`)
         div.innerHTML = " "
-        let p_son = document.createElement('p')
+        let span2_son = document.createElement('p')
         let div2 = document.getElementById(`user-name${i}`)
         div2.innerHTML = " "
         let h5_son = document.createElement('h5')
-        let p_son_time = document.createElement('p')
+        let p_son_time = document.createElement('span')
         let p_son_b = document.createElement('b')
         let user_name = null
         const imgMap = {
@@ -35,15 +35,15 @@ async function last_message() { //Shows recent messages in chat details with sen
         div.appendChild(h5_son)
 
         if (last_m[a].type === "text") {
-            p_son.innerHTML = last_m[a].text
+            span2_son.innerHTML = last_m[a].text
         }
         else {
-            p_son.innerHTML = "link message!!"
+            span2_son.innerHTML = "link message!!"
         }
-        p_son_b.innerHTML = a
-        div.appendChild(p_son)
+        span2_son_b.innerHTML = a
+        div.appendChild(span2_son)
         div.appendChild(p_son_b)
-        div2.appendChild(p_son_time)
+        div.appendChild(p_son_time)
 
     }
 }
