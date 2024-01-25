a42d017529708d98027baa2f5b7e4b9e1bccdc2b
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-01-26 00:15:32 +0200

    up

--- static/QuickChat/files js/last_maessage.js
+++ static/QuickChat/files js/last_maessage.js
@@ -27,7 +27,7 @@ async function last_message() { //Shows recent messages in chat details with sen
         }
         else {
             user_name = last_m[a].user
-            imgMap[i].src = "man.png"
+            imgMap[i].src = "bot.png"
         }
 
         p_son_time.innerHTML = last_m[a].time
