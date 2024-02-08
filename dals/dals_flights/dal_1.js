e37f300d79105972fcd12e0b3a5fc738c8478818
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-02-08 09:54:32 +0200

    Update dal_1.js

--- dals/dals_flights/dal_1.js
+++ dals/dals_flights/dal_1.js
@@ -18,23 +18,23 @@ const connectedKnex = knex({
 
 //new_user
 async function sp_i_users_airlines(uesr) {
-    const new_user = await connectedKnex.raw(`CALL sp_i_users_airlines('${uesr.name}','${uesr.email}','${uesr.password}');`)
+    const new_user = await connectedKnex.raw(`CALL sp_i_users_airlines('${uesr.uesrname}','${uesr.email}','${uesr.password}');`)
     return new_user
 }
 
 //new_user (Automatically generates a password)
 async function sp_pass_users_airlines(uesr) {
-    const new_user = await connectedKnex.raw(`CALL sp_pass_users_airlines('${uesr.name}','${uesr.email}','');`)
+    const new_user = await connectedKnex.raw(`CALL sp_pass_users_airlines('${uesr.uesrname}','${uesr.email}','');`)
     return new_user
 }
 
 async function sp_i_users(uesr) {
-    const new_user = await connectedKnex.raw(`CALL sp_i_users('${uesr.name}','${uesr.email}','${uesr.password}');`)
+    const new_user = await connectedKnex.raw(`CALL sp_i_users('${uesr.uesrname}','${uesr.email}','${uesr.password}');`)
     return new_user
 }
 //new_user (Automatically generates a password)
 async function sp_pass_users(uesr) {
-    const new_user = await connectedKnex.raw(`CALL sp_pass_users('${uesr.name}','${uesr.email}','');`)
+    const new_user = await connectedKnex.raw(`CALL sp_pass_users('${uesr.uesrname}','${uesr.email}','');`)
     return new_user
 }
 
