309d97b8c88e144d02fdff884364a35fbe0513db
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-02-11 17:33:56 +0200

    up

---
+++ routers/flights/role_admins.js
@@ -0,0 +1,174 @@
+const express = require('express')
+const router = express.Router()
+const bl = require('../../bl/bl_role_users')
+
+//filghts_users/users
+
+// GET by ID
+router.get('/users/:id', async (request, response) => {
+    const user_id = parseInt(request.params.id)
+    try {
+        const user = await bl.get_by_id_user(user_id)
+        if (user) {
+            response.json(user)
+        }
+        else {
+            throw response.status(404).json({ "error": `The id ${user_id} you specified does not exist in the system ` })
+
+        }
+
+    } catch (error) {
+        throw response.status(503).json({ "error": `The request failed, try again later ` })
+    }
+})
+
+// POST
+router.post('/users', async (request, response) => {
+    const new_user = request.body
+    try {
+        const result = await bl.create_user(new_user)
+        response.status(201).json(result)
+
+    } catch (error) {
+        throw response.status(409).json({ "error": `Username ${new_user.username} or email ${new_user.email} exist in the system ` })
+        ; // מעבירה את השגיאה הלאה
+    }
+
+})
+
+// PUT 
+router.put('/users/:id', async (request, response) => {
+
+    const user_id = parseInt(request.params.id)
+    const user = await bl.get_by_id_user(user_id)
+
+    if (user) {
+        try {
+            const updated_user_req = request.body
+            const result = await bl.update_user(user_id, updated_user_req)
+            response.json(updated_user_req)
+        }
+        catch (error) {
+            throw response.status(503).json({ "error": `The request failed, try again later  ` })
+            ; // מעבירה את השגיאה הלאה
+        }
+    }
+    else {
+        throw response.status(404).json({ "error": `The id ${user_id} you specified does not exist in the system ` })
+
+    }
+
+})
+
+// DELETE
+router.delete('/users/:id', async (request, response) => {
+    const user_id = parseInt(request.params.id)
+    const user = await bl.get_by_id_user(user_id)
+
+    if (user) {
+        try {
+            const result = await bl.delete_account(user_id)
+            response.status(204).json({ result })
+        }
+        catch (error) {
+            throw response.status(503).json({ "error": `The request failed, try again later  ` })
+            ; // מעבירה את השגיאה הלאה
+        }
+    }
+    else {
+        throw response.status(404).json({ "error": `The ID ${user_id} you specified does not exist ` })
+
+    }
+})
+
+//filghts_users/customers
+
+// GET by ID
+router.get('/customers/:id', async (request, response) => {
+    const user_id = parseInt(request.params.id)
+    const user = await bl.get_by_id_customer(user_id)
+    if (user) {
+        responsestatus(200).json(user)
+    }
+    else {
+        response.status(404).json({ "error": `cannot find user with id ${user_id}` })
+    }
+})
+
+// POST
+router.post('/customers', async (request, response) => {
+    const new_user = request.body
+    const result = await bl.new_customer(new_user)
+    if (user) {
+        responsestatus(201).json(user)
+    }
+    else {
+        response.status(409).json({ "error": `There is a customer with the details I mentioned` })
+    }})
+
+// PUT /PATCH
+router.put('/customers/:id', async (request, response) => {
+    const user_id = parseInt(request.params.id)
+    // user exists ==> perform update
+    const updated_user_req = request.body
+    const result = await bl.update_customer(user_id, updated_user_req)
+    response.json(updated_user_req)
+
+})
+
+//filghts_users/flights
+
+router.get('/flights', async (request, response) => {
+    try {
+        const customers = await bl.get_all_flights()
+        response.json(customers)
+    }
+    catch (e) {
+        response.json({ 'error': JSON.stringify(e) })
+    }
+})
+// GET by ID
+router.get('/flights/:id', async (request, response) => {
+    const user_id = parseInt(request.params.id)
+    const user = await bl.get_by_id_flights(user_id)
+    if (user) {
+        response.json(user)
+    }
+    else {
+        response.status(404).json({ "error": `cannot find user with id ${user_id}` })
+    }
+})
+
+//filghts_users/tickets
+
+// POST
+router.post('/tickets', async (request, response) => {
+    const new_user = request.body
+    const result = await bl.purchase_ticket(new_user)
+    response.status(201).json(result)
+})
+
+//filghts_users/passengers
+
+// POST
+router.post('/passengers', async (request, response) => {
+    const new_user = request.body
+    const result = await bl.new_passenger(new_user)
+    response.status(201).json(result)
+})
+
+// GET by ID
+router.get('/passengers/:id', async (request, response) => {
+    const user_id = parseInt(request.params.id)
+    const user = await bl.get_by_id_passenger(user_id)
+    if (user) {
+        response.status(200).json(user)
+    }
+    else {
+        response.status(404).json({ "error": `cannot find user with id ${user_id}` })
+    }
+})
+
+
+module.exports = router
+
