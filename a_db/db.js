3f692aaf0152586ea44d2f1becb08a7956008564
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-02-11 16:28:21 +0200

    Update db.js

--- a_db/db.js
+++ a_db/db.js
@@ -1,20 +1,20 @@
 const knex = require('knex')
 const config = require('config')
 
-function connect(){
+function connect() {
 
-const connectedKnex = knex({
-    client: 'pg',
-    version: '15',
-    connection: {
-        host: config.db_cloud.host,
-        user: config.db_cloud.user,
-        password: config.db_cloud.password,
-        database: config.db_cloud.database,
-        ssl: true
-    }
-})
-return connectedKnex
+    const connectedKnex = knex({
+        client: 'pg',
+        version: '15',
+        connection: {
+            host: config.db_cloud.host,
+            user: config.db_cloud.user,
+            password: config.db_cloud.password,
+            database: config.db_cloud.database,
+            ssl: true
+        }
+    })
+    return connectedKnex
 }
 
-module.exports = {database}
+module.exports = { connect }
