96a4b1b630d9eaba1382cba78a523a79999ba2e2
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-02-12 22:30:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,6 @@ const express = require('express')
 const cors = require('cors');
 
 const body_parser = require('body-parser')
-const chats_router = require('./routers/chats/chats_router')
-const chat1_router = require('./routers/chats/chat1_router')
-const chat2_router = require('./routers/chats/chat2_router')
-const chat3_router = require('./routers/chats/chat3_router')
-const chat4_router = require('./routers/chats/chat4_router')
-const chat5_router = require('./routers/chats/chat5_router')
-
 const all_tables_router = require('./routers/flights/all_tables_router')
 const role_users = require('./routers/flights/role_users')
 const role_airlines = require('./routers/flights/role_airlines')
@@ -32,12 +25,6 @@ app.listen(3000, () => {
 });
 app.use(cors());
 app.use('/api/all_tables', all_tables_router)
-app.use('/api/chats', chats_router)
-app.use('/api/chat1', chat1_router)
-app.use('/api/chat2', chat2_router)
-app.use('/api/chat3', chat3_router)
-app.use('/api/chat4', chat4_router)
-app.use('/api/connected', chat5_router)
 app.use('/role_admins', role_admins)
 app.use('/role_airlines', role_airlines)
 app.use('/role_users', role_users)
