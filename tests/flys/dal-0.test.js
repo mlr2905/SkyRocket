99234ae58e5240dd9757989795500ba1ddebb3e7
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-02-12 17:05:42 +0200

    Update dal-0.test.js

--- tests/flys/dal-0.test.js
+++ tests/flys/dal-0.test.js
@@ -4,19 +4,19 @@ const dal = require('../../dals/dals_flights/dal_0')
 describe('Testing functionallity of the DAL', () => {
 
     it('get_all', async () => {
-        const expected = 5
-        const messages = await dal.get_all()
-        const actual = messages.length
-        console.log(actual);
+        const expected = 7
+        const all_tables = await dal.get_all()
+
+        const actual = Object.keys(all_tables).length;
         assert.strictEqual(expected, actual)
     })
 
-    // it('get_by_id', async () => {
-    //     const expected = 'bot'
-    //     const message_id_3 = await dal.get_by_id(3)
-    //     const actual = message_id_3.user
-    //     console.log(actual);
-    //     assert.strictEqual(expected, actual)
-    // })
+    
+    it('get_by_id', async () => {
+        const expected = 7
+        const get_by_id = await dal.get_by_id(3)
+        const actual = Object.keys(get_by_id).length;
+        assert.strictEqual(expected, actual)
+    })
 
 })
