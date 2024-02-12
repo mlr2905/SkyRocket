716a9208e16cf221847a98db245b110f48c1eff0
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-02-12 14:21:54 +0200

    Update role_airlines.js

--- routers/flights/role_airlines.js
+++ routers/flights/role_airlines.js
@@ -6,7 +6,16 @@ const bl = require('../../bl/bl_role_users')
 
 router.get('/', async (request, response) => {
     try {
-    const messages = {'message':'Welcome to role airlines the desired path must be specified'}
+    const messages = {'message':'Welcome to role airlines the desired path must be specified,Enter the following path https://cloud-memory.onrender.com/role_users/{neme ?}/1'}
+    response.status(200).json(messages)
+    }
+    catch (error) {
+        throw response.status(503).json({ "error": `The request failed, try again later ${error}` })
+    }
+})
+router.get('/:id', async (request, response) => {
+    try {
+    const messages = {'message':'Enter the following path https://cloud-memory.onrender.com/role_users/{neme ?}/1'}
     response.status(200).json(messages)
     }
     catch (error) {
