e5fae2515b4a2a40472e469c6d153c6d8931cc45
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-02-25 10:25:32 +0200

    Update role_users.js

--- routers/role_users.js
+++ routers/role_users.js
@@ -4,7 +4,32 @@ const qrcode = require('qrcode');
 
 const bl = require('../bl/bl_role_users')
 //role_users/users
-
+/**
+ * @swagger
+ * tags:
+ *   name: Books
+ *   description: The books managing API
+ * /books:
+ *   post:
+ *     summary: Create a new book
+ *     tags: [Books]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses:
+ *       200:
+ *         description: The created book.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       500:
+ *         description: Some server error
+ *
+ */
 /**
 *  @swagger
 *  components:
