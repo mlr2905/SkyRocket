aa157843133daa9a9824b2afcf4ad73fc75fbd14
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-02-27 00:46:52 +0200

    up

---
+++ routers/swagger_role_admins.js
@@ -0,0 +1,13 @@
+
+//categories
+
+
+/**
+ * @swagger
+ * tags:
+ *   name: role_admins
+ *   description: The role_admins managing API
+ */
+
+
+//role_users/admins
---
+++ routers/swagger_role_airlines.js
@@ -0,0 +1,13 @@
+
+//categories
+
+/**
+ * @swagger
+ * tags:
+ *   name: role_airlines
+ *   description: The role_airlines managing API
+ */
+
+
+
+//role_users/airlines
