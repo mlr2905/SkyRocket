2f07a6fc99df624206c5d50f78d207bb92030f99
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-02-26 20:41:22 +0200

    Update role_users.js

--- routers/role_users.js
+++ routers/role_users.js
@@ -263,23 +263,6 @@ router.get('/passengers/:id', async (request, response) => {
 *       credit_card_no:
 *                type: string
 *                example: ************7654
-*       flights:
-*            type: object
-*            required:
-*              - username
-*              - password
-*              - email
-*            properties:
-*              username:
-*                type: string
-*                description: The username of the user.
-*                example: test tsets
-*              password:
-*                type: string
-*                example: test_1
-*              email:
-*                type: string
-*                example: test_tsets@gmail.com
 */
 
 
