78f8c9f8b1ff0a043d4bb0035a15ca0aabc6b9d3
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-02-26 14:53:06 +0200

    Update role_users.js

--- routers/role_users.js
+++ routers/role_users.js
@@ -238,8 +238,6 @@ router.get('/passengers/:id', async (request, response) => {
 *                type: string
 *                example: test_tsets@gmail.com
 *     customers:
-*            type: array
-*            description: An array of customer objects.
 *            items:
 *              type: object
 *              properties:
@@ -264,8 +262,6 @@ router.get('/passengers/:id', async (request, response) => {
 *                  description: The age of the customer.
 *                  example: ************7654
 *       flights:
-*            type: array
-*            description: An array of flight objects.
 *            items:
 *              type: object
 *              properties:
