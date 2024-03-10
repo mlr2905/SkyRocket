4e75b1a88b62028231a934e6e13574c21bac9043
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-03-10 13:34:33 +0200

    up

--- tests/dals/dal-all_tables.test.js
+++ tests/dals/dal-all_tables.test.js
@@ -3,21 +3,22 @@ const dal = require('../../dals/dal_all_tables')
 
 describe('Testing functionallity of the DAL', () => {
 
-    // it('get_all', async () => {
-    //     const expected = 7
-    //     const all_tables = await dal.get_all()
-
-    //     const actual = Object.keys(all_tables).length;
-    //     assert.strictEqual(expected, actual)
-    // })
+    it('get_all', async () => {
+        const expected = 7
+        const all_tables = await dal.get_all()
+console.log(all_tables);
+        const actual = Object.keys(all_tables.rows[0].get_all_data).length;
+        assert.strictEqual(expected, actual)
+    })
 
     
-    // it('get_by_id', async () => {
-    //     const expected = 7
-    //     const get_by_id = await dal.get_by_id(3)
-    //     const actual = Object.keys(get_by_id).length;
-    //     assert.strictEqual(expected, actual)
-    // })
+    it('get_by_id', async () => {
+        const expected = 7
+        const get_by_id = await dal.get_by_id(3)
+        const actual = Object.keys(get_by_id).length;
+        assert.strictEqual(expected, actual)
+    })
+
     it('get_by_id', async () => {
         const expected = 7
         const get_by_id = await dal.get_qr('jcdjjsbsfjebde')
