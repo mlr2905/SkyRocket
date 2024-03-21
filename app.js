605b851df5533eaf62a05cb91befd8a607c28551
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-03-21 10:51:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-
-const logger = require('./logger/my_logger')
-const path = require('path')
-const express = require('express')
+const logger = require('./logger/my_logger');
+const path = require('path');
+const express = require('express');
 const cors = require('cors');
 const basicAuth = require('express-basic-auth');
+const body_parser = require('body-parser');
+const all_tables_router = require('./routers/all_tables');
+const role_users = require('./routers/role_users');
+const role_airlines = require('./routers/role_airlines');
+const role_admins = require('./routers/role_admins');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
+logger.info('==== System start =======');
 
-const body_parser = require('body-parser')
-const all_tables_router = require('./routers/all_tables')
-const role_users = require('./routers/role_users')
-const role_airlines = require('./routers/role_airlines')
-const role_admins = require('./routers/role_admins')
-const swaggerJsdoc = require('swagger-jsdoc')
-const swaggerUi = require('swagger-ui-express')
-logger.info('==== System start =======')
+const users = {'michael': 'Miki260623' };
+const port = 3000;
+
+const checkPassword = (username, password) => { return users[username] === password; };
 
 const options = {
     definition: {
@@ -30,50 +33,27 @@ const options = {
 
 const specs = swaggerJsdoc(options);
 
-const app = express()
+const app = express();
+
 app.use(cors());
-
-const port = 3000
-// // אימות בסיסי
-// const users = {
-//     'admin': '123456'
-// };
-
-// const checkPassword = (username, password) => {
-//     return users[username] === password;
-// };
-
-// // הוספת אימות בסיסי לכל הנתיבים של Swagger UI
-// app.use('/swagger', basicAuth({
-//     users: users,
-//     challenge: true,
-//     unauthorizedResponse: (req) => {
-//         return 'Unauthorized';
-//     },
-//     authorizer: (username, password) => {
-//         return checkPassword(username, password);
-//     }
-// }));
-
-const users = {'michael': 'Miki260623' };
-
-const checkPassword = (username, password) => {return users[username] === password;};
 app.use(basicAuth({
     users: users,
     challenge: true,
-    unauthorizedResponse: (req) => {return 'Unauthorized';},
-    authorizer: (username, password) => {return checkPassword(username, password); }}));
+    unauthorizedResponse: (req) => { return 'Unauthorized'; },
+    authorizer: (username, password) => { return checkPassword(username, password); }
+}));
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(specs));
-app.use(body_parser.json())
-app.use(express.static(path.join('.', '/static/')))
-app.listen(3000, () => {
-    logger.info('==== Server started =======')
+app.use(body_parser.json());
+app.use(express.static(path.join('.', '/static/')));
+
+app.use('/all_tables', all_tables_router);
+app.use('/role_admins', role_admins);
+app.use('/role_airlines', role_airlines);
+app.use('/role_users', role_users);
+
+app.listen(port, () => {
+    logger.info('==== Server started =======');
     console.log('Express server is running ....');
 }); 
-app.use('/all_tables', all_tables_router)
-app.use('/role_admins', role_admins)
-app.use('/role_airlines', role_airlines)
-app.use('/role_users', role_users)
 
-
-logger.info('==== System stop =======')
+logger.info('==== System stop =======');
