ff6258da386d966693d7eb9616f441a675718d9a
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-03-21 10:58:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -53,15 +53,15 @@ const port = 3000
 //     }
 // }));
 
-const users = {'michael': 'Miki260623' };
+// const users = {'michael': 'Miki260623' };
 
-const checkPassword = (username, password) => {return users[username] === password;};
-app.use(cors());
-app.use(basicAuth({
-    users: users,
-    challenge: true,
-    unauthorizedResponse: (req) => {return 'Unauthorized';},
-    authorizer: (username, password) => {return checkPassword(username, password); }}));
+// const checkPassword = (username, password) => {return users[username] === password;};
+// app.use(cors());
+// app.use(basicAuth({
+//     users: users,
+//     challenge: true,
+//     unauthorizedResponse: (req) => {return 'Unauthorized';},
+//     authorizer: (username, password) => {return checkPassword(username, password); }}));
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(specs));
 app.use(body_parser.json())
 app.use(express.static(path.join('.', '/static/')))
