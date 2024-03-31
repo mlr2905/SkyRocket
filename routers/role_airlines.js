b8d2ae97d9cda97a0c4798ce115976a9e4fc7930
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-03-31 13:02:23 +0300

    Update role_airlines.js

--- routers/role_airlines.js
+++ routers/role_airlines.js
@@ -269,10 +269,10 @@ router.put('/flights/:id', async (request, response) => {
                 response.json(id, update_flight)
             }
             if (result.status == "some") {
-                response.status(404).json({ "error": `The id ${update_flight.result.status} you specified does not exist in the ${result.status}` })
+                response.status(404).json({ "error": `The id ${update_flight} you specified does not exist in the ${result.status}` })
             }
             if (result.status == "plane_id" || result.status === "origin_country_id" || result.status === "destination_country_id" || result.status === "airline_id") {
-                response.status(404).json({ "error": `The id ${result.status} you specified does not exist in the ${result.status}` })
+                response.status(404).json({ "error": `The id ${update_flight} you specified does not exist in the ${result.status}` })
             }
             if (result.status == "exists") {
                 response.status(409).json({ "error": `${result.status} The flight you want already exists` })
