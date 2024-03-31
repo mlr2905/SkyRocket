62607110a237af9baccfe3311fc586c09aa4da45
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-03-31 13:17:48 +0300

    Update role_airlines.js

--- routers/role_airlines.js
+++ routers/role_airlines.js
@@ -272,7 +272,7 @@ router.put('/flights/:id', async (request, response) => {
                 response.status(404).json({ "error": `The id ${update_flight} you specified does not exist in the ${result.status}` })
             }
             if (result.status == "plane_id" || result.status === "origin_country_id" || result.status === "destination_country_id" || result.status === "airline_id") {
-               let name = result.status == "plane_id" ? "plane_id": result.status === "origin_country_id"? "origin_country_id":result.status === "destination_country_id"?"destination_country_id": result.status === "airline_id"? "airline_id":null
+               let name = result.status == "plane_id" ? plane_id: result.status === "origin_country_id"? origin_country_id:result.status === "destination_country_id"?destination_country_id: result.status === "airline_id"? airline_id:null
                 response.status(404).json({ "error": `The id ${update_flight.name} you specified does not exist in the ${result.status}` })
             }
             if (result.status == "exists") {
