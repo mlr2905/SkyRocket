36e551638bcde376c3e64f805a3f3c3ef7d8df95
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-06-03 11:11:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -148,6 +148,7 @@ passport.use(new GitHubStrategy({
     callbackURL: "https://skyrocket.onrender.com/git"
   },
   function(accessToken, refreshToken, profile, done) {
+    console.log("profile שש",profile);
     return done(null, profile);
   }
 ));
@@ -171,67 +172,7 @@ app.get('/git',
 passport.authenticate('github', { scope: [ 'read:user', 'user:email' ] }),
 
 function(req, res){
-    async function (req, res) {
-        const { profile, accessToken } = req.user;
-        let email = profile.emails[0].value;
-        let password = profile.nodeId; // ניתן לשנות את זה בהתאם לצורך
-
-        try {
-            console.log("ll", email);
-            const Check = await axios.get(`https://skyrocket.onrender.com/role_users/users/search?email=${email}`);
-            const data = Check.data;
-
-            let loginResponse;
-            if (data.e === "no") {
-                console.log("aaa");
-                // אם המייל קיים, בצע login
-                loginResponse = await axios.post('https://skyrocket.onrender.com/role_users/login', {
-                    email: email,
-                    password: password
-                });
-                const token = loginResponse.data.jwt;
-
-                console.log("token", token);
-                return res.cookie('sky', token, {
-                    httpOnly: true,
-                    sameSite: 'strict',
-                    maxAge: (3 * 60 * 60 * 1000) + (15 * 60 * 1000) // 3 שעות ו־2 דקות במילישניות
-                }),
-                    res.redirect('https://skyrocket.onrender.com/swagger');
-            // הפנה לדף הבית או לכל דף אחר לאחר ההתחברות
-        } else if (data.e === "noo") {
-            console.log("aa");
-            // אם המייל לא קיים, בצע signup ואז login
-            const signup = await axios.post('https://skyrocket.onrender.com/role_users/signup', {
-                email: email,
-                password: password
-            });
-            if (signup.data.e === "no") {
-                loginResponse = await axios.post('https://skyrocket.onrender.com/role_users/login', {
-                    email: email,
-                    password: password
-                });
-
-                const token = loginResponse.data.jwt;
-
-                console.log("token", token);
-                return res.cookie('sky', token, {
-                    httpOnly: true,
-                    sameSite: 'strict',
-                    maxAge: (3 * 60 * 60 * 1000) + (15 * 60 * 1000) // 3 שעות ו־2 דקות במילישניות
-                }),
-                    res.redirect('https://skyrocket.onrender.com/swagger');
-
-            }
-        }
-
-    } catch (error) {
-        console.error('Error during signup or login:', error);
-        res.status(500).send('Error during signup or login');
-    }
-    }
-
-
+ 
   });
 
 
