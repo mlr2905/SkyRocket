8f81348208835acd9a4c552e5fab2da41793e936
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-06-04 15:04:45 +0300

    Update facebook_auth.js

--- OAuth/facebook_auth.js
+++ OAuth/facebook_auth.js
@@ -4,23 +4,20 @@ const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
 const session = require('express-session');
 
-const qs = require('qs');
-
-
 // קונפיגורציה
 const facebook_appId = '1156854418774447';
-const facebook_appSecret = '7bd06b4a0356ba8fba5ef8365f37e718';
+const facebook_appSecret = '7bd06b4a0356ba8fba5ef8365f37e718'; // החלף במפתח הסודי שלך
 
 function facebook_auth(app) {
+
   passport.use(new FacebookStrategy({
       clientID: facebook_appId,
       clientSecret: facebook_appSecret,
-      callbackURL:'https://skyrocket.onrender.com/facebook',  
-      profileFields: ['id','displayName','name','first_name','last_name','middle_name','short_name','gender',
-        'email','birthday','friends','hometown','location','link','locale','timezone','photos',
-        'picture.type(large)','cover','updated_time','verified'
+      callbackURL: 'https://skyrocket.onrender.com/facebook/callback',
+      profileFields: ['id', 'displayName', 'name', 'first_name', 'last_name', 'middle_name', 'short_name', 'gender',
+        'email', 'birthday', 'friends', 'hometown', 'location', 'link', 'locale', 'timezone', 'photos',
+        'picture.type(large)', 'cover', 'updated_time', 'verified'
       ]
-
     },
     function(accessToken, refreshToken, profile, done) {
       console.log('User Profile:', profile); // הדפסת המידע בקונסול
@@ -37,7 +34,7 @@ function facebook_auth(app) {
   });
 
   // הגדרת סשנים
-  app.use(require('express-session')({ secret: 'your_secret_key', resave: false, saveUninitialized: true }));
+  app.use(session({ secret: 'your_secret_key', resave: false, saveUninitialized: true }));
 
   // הגדרת Passport
   app.use(passport.initialize());
@@ -45,9 +42,10 @@ function facebook_auth(app) {
 
   // הגדרת מסלולים לאימות
   app.get('/facebook', passport.authenticate('facebook', {
-    scope: ['email','user_birthday','user_friends','user_gender','user_hometown','user_location','user_likes',
-    'user_photos','user_posts','user_videos']
+    scope: ['email', 'user_birthday', 'user_friends', 'user_gender', 'user_hometown', 'user_location', 'user_likes',
+    'user_photos', 'user_posts', 'user_videos']
   }));
+
   app.get('/facebook/callback',
     passport.authenticate('facebook', { failureRedirect: '/' }),
     function(req, res) {
