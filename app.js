a9d7a119283f80d36141f62c87b0f0a802442914
mlr2905 <7585474@gmail.com>
2025-05-15 10:19:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,169 @@
-const express = require('express');
-const logger = require('./logger/my_logger');
-const setupMiddleware = require('./config/middleware');
-const { setupAuthRoutes } = require('./routes/authRoutes');
-const setupSwagger = require('./config/swaggerConfig');
+const express = require('express')
+const session = require('express-session');
+const moment = require('moment-timezone');
+const logger = require('./logger/my_logger')
+const path = require('path')
+const cors = require('cors');
+const axios = require('axios');
+const body_parser = require('body-parser')
 const all_tables_router = require('./routes/all_tables')
 const role_users = require('./routes/role_users')
 const role_admins = require('./routes/role_admins')
 const role_airlines = require('./routes/role_airlines')
+const swaggerJsdoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+const app = express()
+
+const passport = require('passport');
+const OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
+const { handleGoogleLogin } = require('./auth/Google_Auth');  
+const { handleGitHubLogin } = require('./auth/Git_Auth');  // ייבוא הפונקציה
 require('dotenv').config();
+app.use(session({ secret: 'keyboard', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-// הודעת לוג בעת טעינת מודולים
-logger.info('==== System start =======');
-logger.info('Loading modules and initializing server...');
 
-const app = express();
 
-// הגדרת מידלוואר
-setupMiddleware(app);
+logger.info('==== System start =======')
+app.use(cors());
+app.listen(9000, () => {
+    
+    console.log('localhost:9000');
 
-// הגדרת נתיבי אימות והאימות עצמו
-setupAuthRoutes(app);
+    console.log('Express server is running ....');
+});
 
-// הגדרת Swagger
-setupSwagger(app);
 
-// הגדרת נתיבים אחרים
+
+
+app.get('/google',
+    passport.authenticate('google', { scope: ['profile', 'email', 'openid'] }),
+    handleGoogleLogin  // קריאה לפונקציה שהובאה מ-googleAuth.js
+);
+
+
+
+// ניתוב לאימות GitHub
+app.get('/git',
+    passport.authenticate('github', { scope: ['read:user', 'user:email', 'user:read:email'] }),
+    handleGitHubLogin  
+);
+
+
+        
+
+app.get('/activation', async (req, res) => {
+    try {
+
+        const cookies = req.headers.cookie.split(';').map(cookie => cookie.trim());
+        const skyToken = cookies.find(cookie => cookie.startsWith('sky='));
+        if (!skyToken) {
+            res.status(404).json('on')
+            console.log('erre');
+        }
+        else {
+            res.status(200).json('ok');
+            console.log('ok');
+
+        }
+    } catch (e) {
+        return res.status(500).json(e)
+    }
+});
+
+app.get('/logout', async (req, res) => {
+    try {
+
+        res.clearCookie('sky');
+        return redirectToLogin(req, res);
+
+
+    } catch (e) {
+        return res.status(500).json(e)
+    }
+});
+
+
+app.get('/', async (req, res, next) => {
+    try {
+
+
+        if (req.path === '/login.html') {
+            return next()
+        }
+
+        if (req.path === '/search_form.html') {
+            return next()
+        }
+        if (req.path === '/registration.html') {
+            return next()
+        }
+        const cookies = req.headers.cookie.split(';').map(cookie => cookie.trim());
+        const skyToken = cookies.find(cookie => cookie.startsWith('sky='));
+        if (!skyToken) {
+            return redirectToLogin(req, res);
+        }
+        const token = skyToken.split('=')[1];
+        const response = await axios.get('https://jwt-node-mongodb.onrender.com/data', {
+            data: { token }
+        });
+        const data = response.data;
+        if (data.valid) {
+
+            // הגדרת השעה הנוכחית של השרת לשעה ישראלית
+            const israelTime = moment.tz(Date.now(), 'Asia/Jerusalem');
+
+            // השתמש בזמן השרת כזמן התחלה עבור העוגיה
+            res.clearCookie('sky');
+            res.cookie('sky', token, {
+                httpOnly: true,
+                sameSite: 'strict',
+                expires: israelTime.add(3, 'hours').add(15, 'minutes').toDate()
+            });
+            next();
+        } else {
+            return res.status(200).redirect(302, './login.html');
+        }
+    } catch (e) {
+        return res.status(500).send({ "error": e, "message": 'Internal Server Error' });
+    }
+});
+
+
+function redirectToLogin(req, res) {
+    res.status(200).send(`
+        <script>
+            document.cookie = 'redirect=${req.originalUrl}; max-age=3600';
+            window.location.href = 'https://skyrocket.onrender.com/login.html';
+        </script>
+    `);
+}
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "SkyRocket API",
+            version: "1.0.1",
+            description: "My REST API SkyRocket",
+        },
+        cors: {
+            origin: '*',
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+        },
+        servers: [{ url: "https://skyrocket.onrender.com/" }],
+    },
+    apis: ["./swagger/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(specs));
+app.use(body_parser.json())
+app.use(express.static(path.join('.', '/static/')))
 app.use('/all_tables', all_tables_router)
 app.use('/role_admins', role_admins)
 app.use('/role_airlines', role_airlines)
 app.use('/role_users', role_users)
-// התחלת השרת
-app.listen(9000, () => {
-    logger.info('Server started successfully on port 9000');
-    logger.info('Express server is running at localhost:9000');
-});
 
-// הודעת סיכום על סיום טעינת השרת
-logger.info('Server configuration completed successfully');
