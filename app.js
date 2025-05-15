93ab16b8030cb2440db61e68fcc6d5d844b6fb4f
mlr2905 <7585474@gmail.com>
2025-05-15 10:15:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const logger = require('./logger/my_logger');
 const setupMiddleware = require('./config/middleware');
 const { setupAuthRoutes } = require('./routes/authRoutes');
 const setupSwagger = require('./config/swaggerConfig');
-const setupRoutes = require('./routes');
+const all_tables_router = require('./routes/all_tables')
+const role_users = require('./routes/role_users')
+const role_admins = require('./routes/role_admins')
+const role_airlines = require('./routes/role_airlines')
 require('dotenv').config();
 
 // הודעת לוג בעת טעינת מודולים
@@ -22,8 +25,10 @@ setupAuthRoutes(app);
 setupSwagger(app);
 
 // הגדרת נתיבים אחרים
-setupRoutes(app);
-
+app.use('/all_tables', all_tables_router)
+app.use('/role_admins', role_admins)
+app.use('/role_airlines', role_airlines)
+app.use('/role_users', role_users)
 // התחלת השרת
 app.listen(9000, () => {
     logger.info('Server started successfully on port 9000');
