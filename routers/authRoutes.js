47a8cff0267cc75febade2625bc90be4c6616b99
mlr2905 <7585474@gmail.com>
2025-05-15 09:52:25 +0300

    Update authRoutes.js

--- routers/authRoutes.js
+++ routers/authRoutes.js
@@ -2,8 +2,8 @@ const passport = require('passport');
 const axios = require('axios');
 const moment = require('moment-timezone');
 const logger = require('../logger/my_logger');
-const { handleGoogleLogin } = require('./auth/Google_Auth');
-const { handleGitHubLogin } = require('./auth/Git_Auth');
+const { handleGoogleLogin } = require('../auth/Google_Auth');
+const { handleGitHubLogin } = require('../auth/Git_Auth');
 
 function setupAuthRoutes(app) {
     // ניתובים לאימות
