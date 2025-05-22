0c14cc756dc15ec3a61064caa87311ee396fc62c
mlr2905 <7585474@gmail.com>
2025-05-22 15:05:19 +0300

    Update login.js

--- client/public/js/login.js
+++ client/public/js/login.js
@@ -1,5 +1,5 @@
-const API_REG_URL = "https://skyrocket.onrender.com/loginWebAuthn";
-const API_LOGIN_URL = "https://skyrocket.onrender.com/signupWebAuthn";
+const API_LOGIN_URL = "https://skyrocket.onrender.com/loginWebAuthn";
+const API_REG_URL = "https://skyrocket.onrender.com/signupWebAuthn";
 
 // משתנים גלובליים
 let isWebAuthnSupported = false;
