6b5ce02084797a675802337a9458fc4697538ceb
mlr2905 <7585474@gmail.com>
2025-11-07 01:13:10 +0200

    Update constants.js

--- client/public/js/utils/constants.js
+++ client/public/js/utils/constants.js
@@ -1,17 +1,17 @@
 // File: constants.js
 
 // --- Login Endpoints ---
-export const API_LOGIN_URL = "https://skyrocket.onrender.com/role_users/loginWebAuthn";
-export const API_REG_URL = "https://skyrocket.onrender.com/role_users/signupwebauthn";
-export const API_AUTH_CODE_URL = "https://skyrocket.onrender.com/role_users/authcode";
-export const API_VALIDATE_CODE_URL = "https://skyrocket.onrender.com/role_users/validation";
-export const API_LOGIN_PASSWORD_URL = "https://skyrocket.onrender.com/role_users/login";
-export const GOOGLE_AUTH_URL = "https://skyrocket.onrender.com/google";
-export const GIT_AUTH_URL = "https://skyrocket.onrender.com/git";
-export const LOGIN_PAGE_URL = "https://skyrocket.onrender.com/login.html";
+export const API_LOGIN_URL = "role_users/loginWebAuthn";
+export const API_REG_URL = "role_users/signupwebauthn";
+export const API_AUTH_CODE_URL = "role_users/authcode";
+export const API_VALIDATE_CODE_URL = "role_users/validation";
+export const API_LOGIN_PASSWORD_URL = "role_users/login";
+export const GOOGLE_AUTH_URL = "google";
+export const GIT_AUTH_URL = "/git";
+export const LOGIN_PAGE_URL = "login.html";
 
 // --- Registration Endpoints ---
-export const API_IP_URL = "https://skyrocket.onrender.com/role_users/ip";
+export const API_IP_URL = "role_users/ip";
 export const API_SIGNUP_URL = "/role_users/signup";
 export const API_CUSTOMERS_URL = "/role_users/customers";
 export const API_USER_SEARCH_URL = "role_users/users/search";
