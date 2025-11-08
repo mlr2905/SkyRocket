d09becc598d40e218420d9201e0d4d9924bfd45b
mlr2905 <7585474@gmail.com>
2025-11-08 22:24:44 +0200

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -5,13 +5,13 @@ const logger = require('../logger/my_logger');
 const protect = async (req, res, next) => {
     logger.debug('Running protection middleware');
     let token;
-        console.log("req.cookies.sky",req.cookies.sky);
+    console.log("req.cookies.sky", req.cookies.sky);
 
     // 1. נסה לקרוא את העוגייה המאובטחת
     if (req.cookies && req.cookies.sky) {
-        
+
         token = req.cookies.sky;
-        console.log("token",token);
+        console.log("token", token);
 
     }
 
@@ -21,17 +21,17 @@ const protect = async (req, res, next) => {
     }
 
     try {
-     const response = await axios.post('http://localhost:3001/data', {
+        const response = await axios.post('https://jwt-node-mongodb.onrender.com/data', {
             token: token // שלח את הטוקן ישירות כאובייקט
         });
-console.log("response.data",response.data);
+        console.log("response.data", response.data);
 
         if (response.data.valid) {
             // 3. הוסף את פרטי המשתמש המפוענחים לבקשה
             //    הטוקן מכיל את ה-ID והמייל (לפי bl_role_users.js)
             req.user = response.data.user; // נניח שהאוביקט חוזר כ- { user: { id: 49, email: '...' } }
-            console.log("req.user",req.user);
-            
+            console.log("req.user", req.user);
+
             logger.debug(`User authorized: ${req.user.email} (ID: ${req.user.id})`);
             next(); // המשך לבקר (Controller)
         } else {
